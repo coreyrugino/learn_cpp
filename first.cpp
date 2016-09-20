02bd3e1989b9c03d96c4fa24f35348c995799b96
Corey Rugino <coreyrugino@gmail.com>
2016-09-19 20:13:35 -0600

    initial push to cpp

---
+++ first.cpp
@@ -0,0 +1,16 @@
+//
+//  first.cpp
+//
+//
+//  Created by Dream Machine on 9/19/16.
+//
+//
+
+//#include "first.hpp"
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
